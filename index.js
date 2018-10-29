2ba9e3ad677f973df695494516472be2509c7fb1
Adam Duff <j.adam.duff@gmail.com>
2018-10-29 03:03:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,13 @@ class Driver {
 
     store.drivers.push(this);
   }
-
+  trips() {
+    return store.trips.filter(
+      function(trip) {
+        trip.driverId === this.id;
+      }.bind(this)
+    );
+  }
 }
 
 class Passenger {
