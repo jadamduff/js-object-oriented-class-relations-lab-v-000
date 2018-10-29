acf0a8392b7f15c9c3335b3df99a847a86caf29d
Adam Duff <j.adam.duff@gmail.com>
2018-10-29 02:36:55 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,23 @@
+let store = {
+  drivers: [],
+  passengers: [],
+  trips: []
+}
+
+let driverId = 0;
+let passengerId = 0;
+let tripId = 0;
+
+class Driver = {
+  constructor(name) {
+    this.name = name;
+    this.id = ++driverId;
+  }
+}
+
+class Passenger = {
+  constructor(name) {
+    this.name = name;
+    this.id = ++passengerId;
+  }
+}
